9b5b8b6efd3b0ec40c27ee66bd378ee329522a40
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-07-25 06:15:51 +0000

    fixing missing requirement

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -9,6 +9,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	int right_height, left_height;
 
+	if (tree == NULL)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
